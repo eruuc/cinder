22c379b1c3e823383f07f4df0fbe1600ac940eb1
eruuc <kim.eric3@northeastern.edu>
2024-02-11 13:00:54 -0500

    Updated Project
    updated project to include TypeScript files

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,33 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import project1Logo from '/images/project1.jpeg'
+import project2Logo from '/images/project2.jpg'
+import project3Logo from '/images/project3.jpg'
+import project4Logo from '/images/project4.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className = 'projects-box'>
+            <div className = 'projects'>
+                <p>1. Project Name </p>   
+                <img className = 'thumbnail' src = {project1Logo}></img>
+                <p>Contributor/Contributors: John</p>  
+                <p>Description: Insert Description Here</p>  
+                <p>Would you like to participate in this project?</p>       
+                <button className = 'yes-button'>Yes</button>     
+                <button className = 'no-button'>No</button>
+            </div>
+
+            <div className = 'projects'>
+                <p>2. Project Name </p>   
+                <img className = 'thumbnail' src = {project2Logo}></img>
+                <p>Contributor/Contributors: Sally</p>  
+                <p>Description: Insert Description Here</p>  
+                <p>Would you like to participate in this project?</p>       
+                <button className = 'yes-button'>Yes</button>     
+                <button className = 'no-button'>No</button>
+            </div>
+            
+            <div className = 'projects'>
+                <p>3. Project Name </p>   
+                <img className = 'thumbnail' src = {project3Logo}></img>
+                <p>Contributor/Contributors: Sally</p>  
+                <p>Description: Insert Description Here</p>  
+                <p>Would you like to participate in this project?</p>       
+                <button className = 'yes-button'>Yes</button>     
+                <button className = 'no-button'>No</button>
+            </div>
+
+            <div className = 'projects'>
+                <p>4. Project Name </p>   
+                <img className = 'thumbnail' src = {project4Logo}></img>
+                <p>Contributor/Contributors: Sally</p>  
+                <p>Description: Insert Description Here</p>  
+                <p>Would you like to participate in this project?</p>       
+                <button className = 'yes-button'>Yes</button>     
+                <button className = 'no-button'>No</button>
+            </div>
+        </div>
     </>
   )
 }
